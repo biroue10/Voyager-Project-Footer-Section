54d2529a36544a43f6f721260ce614f64b032f2e
BIROUE ISAAC <biroueisaac@gmail.com>
2023-08-18 11:40:19 +0200

    First step taken in my React project! 🚀 #InitialCommit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+import "./App.css";
+import Footer from "./components/Footer";
+
+=======
+import './App.css'
+import Header from './components/Header'
+import Landing from './components/Landing'
+>>>>>>> c9a204a23faeafe2d87218c308a3a7e205a87d77
+function App() {
+  return (
+<<<<<<< HEAD
+    <>
+      <h1>Fireball</h1>
+      <Footer />
+    </>
+  );
+=======
+    <div className='app w-[100vw] h-[100vh] overflow-x-hidden overflow-y-scroll relative'>
+      
+      <Header />
+      <Landing />
+
+    </div>
+  )
+>>>>>>> c9a204a23faeafe2d87218c308a3a7e205a87d77
+}
+
+export default App;
